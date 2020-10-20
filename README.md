3d603aadf5b2a929f5a5f0e5c87b6c84340d9108
Genki Furukawa <genki.furukawa@gmail.com>
2020-10-20 23:58:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StreamingResponseBody サンプル
+zipファイルを作成してダウンロードするための調査・デモ用のレポジトリ。
+ブラウザから`http://localhost:8080/dowload`にアクセスすることで、zipファイルでストリーミングダウンロードが開始される。
---
+++ src/main/java/streaming/response/body/demo/controller/DownloadController.java
@@ -0,0 +1,68 @@
+package streaming.response.body.demo.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@RestController
+@RequestMapping("download")
+public class DownloadController {
+
+    /**
+     * zipファイルを生成しながら出力するサンプル
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<StreamingResponseBody> download() {
+
+        // zipを作成しながらresponseを返す
+        StreamingResponseBody responseBody = out -> {
+            ZipOutputStream zipOutputStream = new ZipOutputStream(out);
+
+            try {
+                File f = new File("/Users/genki/Desktop/test.jpg");
+                InputStream inputStream = new FileInputStream(f);
+
+                // パスをしてしてやると、
+                ZipEntry zipEntry = new ZipEntry("sample/" + f.getName());
+                zipOutputStream.putNextEntry(zipEntry);
+
+                byte[] bytes=new byte[1024];
+                int length;
+                while ((length = inputStream.read(bytes)) >= 0) {
+                    zipOutputStream.write(bytes, 0, length);
+                }
+
+                inputStream.close();
+                zipOutputStream.close();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        // レスポンスヘッダを作成
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "application/zip");
+        // zipファイルの名前を指定できる
+        responseHeaders.set("Content-Disposition", "attachment; filename=test.zip");
+
+        ResponseEntity<StreamingResponseBody> response = new ResponseEntity<>(
+                responseBody,
+                responseHeaders,
+                HttpStatus.OK);
+
+        return response;
+    }
+}
